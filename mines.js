7d6195a631382d0a7962f728c8c02aabf15a3603
Gour Adhikary <157149036+anonymousthinker@users.noreply.github.com>
2024-11-20 01:12:57 +0530

    Update mines.js

--- mines.js
+++ mines.js
@@ -1,12 +1,13 @@
 const MINE_ONE = Math.floor((Math.random()) * 10);
 const MINE_TWO = Math.floor((Math.random()) * 10);
+const FIELD_LENGTH = 40;
 
 function takePlayerInput() {
   return prompt('Cell number:');
 }
 
 function createHeader(header) {
-  for (let index = 1; index < 40; index++) {
+  for (let index = 1; index < FIELD_LENGTH; index++) {
     header += '━';
   }
 
@@ -14,7 +15,7 @@ function createHeader(header) {
 }
 
 function createFooter(footer) {
-  for (let index = 1; index < 40; index++) {
+  for (let index = 1; index < FIELD_LENGTH; index++) {
     footer += '━';
   }
 
@@ -22,24 +23,27 @@ function createFooter(footer) {
 }
 
 function createField(middle) {
-  for (let index = 1; index <= 10; index++) {
+  for (let index = 1; index <= FIELD_LENGTH / 4; index++) {
     middle += '┃   ';
   }
 
   return middle + '┃';
 }
 
+function updateFieldContent(middle, playerInput, mineFound, index) {
+  if (middle[index] === ' ' && index === playerInput * 4 + 1) {
+    return mineFound ? '☠︎︎' : '♦';
+  }
+
+  return middle[index];
+}
+
 function updateField(middle, playerInput, mineFound) {
   let updatedMiddle = '';
   let index = 0;
-  while(index <= 40) {
-    if (middle[index] === ' ' && index === playerInput * 4 + 1) {
-      updatedMiddle += mineFound ? '☠︎︎' : '♦';
-      index += 1;
-      continue;
-    }
 
-    updatedMiddle += middle[index];
+  while (index <= FIELD_LENGTH) {
+    updatedMiddle += updateFieldContent(middle, playerInput, mineFound, index);
     index += 1;
   }
 
